a0811402dd9733e7ae17637d5b3205507d686bf4
Ilyakredo <imoiseenko25@ukr.net>
2023-09-17 23:59:24 -0600

    personal ws - changes 17 september 2023

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -1,4 +1,4 @@
-export const PHONE_NUMBER = "+38(050)974-38-46";
+export const PHONE_NUMBER = "+1(368)299-3665";
 export const EMAIL_SUCCESS_MSG = "Your message has been successfully sent.";
 export const EMAIL_FAIL_MSG = "Error. Message not sent.";
 export const DEFAULT_TIMEOUT = 3000;
--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -101,7 +101,7 @@ export const qualityDescriptionArr = [
 export const detailsArr = [
   { id: 1, detailDesc: "Name", detailValue: "Illia Moiseienko" },
   { id: 2, detailDesc: "Email", detailValue: "illia.moiseienko@gmail.com" },
-  { id: 3, detailDesc: "Phone", detailValue: "+38(050) 974-38-46" },
+  { id: 3, detailDesc: "Phone", detailValue: "+1(368)299-3665" },
   { id: 4, detailDesc: "Location", detailValue: "Calgary, AB, Canada" },
 ];
 
